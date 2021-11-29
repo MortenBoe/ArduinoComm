e2728b3779b4ec7bc3664306908da669fd4a88d7
MortenB <61787215+MortenB@users.noreply.github.com>
2021-11-29 08:47:14 +0100

    Add project files.

---
+++ ArduinoComm/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace ArduinoComm
+{
+    partial class frmArduino
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOn = new System.Windows.Forms.Button();
+            this.btnOff = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.ArduinoPort = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblPort = new System.Windows.Forms.Label();
+            this.btnLesData = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnOn
+            // 
+            this.btnOn.Location = new System.Drawing.Point(26, 77);
+            this.btnOn.Name = "btnOn";
+            this.btnOn.Size = new System.Drawing.Size(113, 23);
+            this.btnOn.TabIndex = 0;
+            this.btnOn.Text = "Test  \"A\"";
+            this.btnOn.UseVisualStyleBackColor = true;
+            this.btnOn.Click += new System.EventHandler(this.btnOn_Click);
+            // 
+            // btnOff
+            // 
+            this.btnOff.Location = new System.Drawing.Point(26, 106);
+            this.btnOff.Name = "btnOff";
+            this.btnOff.Size = new System.Drawing.Size(113, 23);
+            this.btnOff.TabIndex = 1;
+            this.btnOff.Text = "Test \"a\"";
+            this.btnOff.UseVisualStyleBackColor = true;
+            this.btnOff.Click += new System.EventHandler(this.btnOff_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(145, 204);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(29, 47);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(72, 24);
+            this.cmbPort.TabIndex = 3;
+            this.cmbPort.SelectedIndexChanged += new System.EventHandler(this.cmbPort_SelectedIndexChanged);
+            // 
+            // ArduinoPort
+            // 
+            this.ArduinoPort.PortName = "COM_<Arduino>";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(29, 25);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(31, 16);
+            this.lblPort.TabIndex = 4;
+            this.lblPort.Text = "Port";
+            // 
+            // btnLesData
+            // 
+            this.btnLesData.Location = new System.Drawing.Point(64, 203);
+            this.btnLesData.Name = "btnLesData";
+            this.btnLesData.Size = new System.Drawing.Size(75, 23);
+            this.btnLesData.TabIndex = 5;
+            this.btnLesData.Text = "LesData";
+            this.btnLesData.UseVisualStyleBackColor = true;
+            this.btnLesData.Click += new System.EventHandler(this.btnLesData_Click);
+            // 
+            // frmArduino
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnLesData);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.cmbPort);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnOff);
+            this.Controls.Add(this.btnOn);
+            this.Name = "frmArduino";
+            this.Text = "Arduino";
+            this.Load += new System.EventHandler(this.frmArduino_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOn;
+        private System.Windows.Forms.Button btnOff;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.IO.Ports.SerialPort ArduinoPort;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Button btnLesData;
+    }
+}
+
---
+++ ArduinoComm/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+
+
+namespace ArduinoComm
+{
+    public partial class frmArduino : Form
+    {
+        public frmArduino()
+        {
+            InitializeComponent();
+        }
+
+        private float Data(String data, String varnavn)
+        {
+            String[] parts = data.Split(';');
+            /*
+            foreach (var navn in parts)
+            {
+                if navn.StartsWith "Temp="
+
+            }
+            
+            */
+            return 0;
+        }
+        private void frmArduino_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                if (SerialPort.GetPortNames().Length > 0)
+                {
+                    lblPort.Text = "Arduino tilkoblet på port:";
+                    cmbPort.Items.AddRange(SerialPort.GetPortNames());
+                    cmbPort.SelectedIndex = 0;
+                    ArduinoPort.BaudRate = 9600;
+
+                    ArduinoPort.PortName = cmbPort.Text;
+                }
+                else
+                    lblPort.Text = "Ingen Arduino tilkoblet.";
+       
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnOn_Click(object sender, EventArgs e)
+        {
+            try {
+
+                ArduinoPort.Open();
+                ArduinoPort.Write("A");
+                ArduinoPort.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void btnOff_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ArduinoPort.Open();
+                ArduinoPort.Write("a");
+                ArduinoPort.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void cmbPort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ArduinoPort.PortName = cmbPort.Text;
+
+        }
+
+        private void btnLesData_Click(object sender, EventArgs e)
+        {
+             
+        }
+
+
+
+
+    }
+}
+/*
+ * 
+ 
+//Arduino-kode:
+
+int PERIODE1 = 500;   
+unsigned long TID1 = 0;
+
+int PERIODE2 = 333;  
+unsigned long TID2 = 0; 
+
+bool LEDstatus = false;
+
+ 
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(13,OUTPUT);
+}
+ 
+void loop() 
+{
+
+      // leser innkomne data fra PC
+      if (Serial.available()) {
+        String d = Serial.readString();
+        char c = d.charAt(0);
+        
+            switch(c) {
+                case 'A':
+                    PERIODE2 = 100;  // blinker i høyere takt
+                    //digitalWrite(13,HIGH);  // Hvis man ønsker å skru på
+                break;
+                
+                case 'a':
+                    PERIODE2 = 500; // blinker i lavere takt
+                     //digitalWrite(13,LOW);  // Hvis man ønsker å skru av
+                break;
+                
+               // F.eks. S120 blir her omgjort til   value=120
+               // case 'S':
+                //   String x = data.substring(1);
+                //   int value = x.toInt();
+               // break;
+             // 
+             }
+      }
+
+      
+   // PERIODE 1
+    if (millis() >= TID1 + PERIODE1)
+{
+    TID1 += PERIODE1;
+
+    // Utfør noe etter periode 1
+    // Her skriver vi ut noen vilkårlige testdata til PC som kan være hentet fra ulike sensorer
+    // F.eks: "A:;Temp=16;D1=39;D2=10.83"
+    int temp = random(13, 25); // Tilfeldig temperatur
+    int d1 = random(0, 255);  // tilfeldig tall
+
+    String data = "A:";
+    data += "Temp=";
+    data += temp;
+    data += ";D1=";
+    data += d1;
+    data += ";D2=";
+    data += d1 / 3.6;
+
+    Serial.println(data);  // skriver data til PC
+
+}
+
+// PERIODE2:
+if (millis() >= TID2 + PERIODE2)
+{
+    TID2 += PERIODE2;
+
+    // Vi lan LED13 blinke av/på 
+    LEDstatus = !LEDstatus;
+    digitalWrite(13, LEDstatus);
+}
+ 
+}
+*/
